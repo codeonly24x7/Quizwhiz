77c4ac8ab5a7ff5d6fe5e470c57762a2a1a1d978
nikh2901 <nikhilanurag00@gmail.com>
2026-01-31 21:12:15 +0530

    add game and show score

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         QuestionService qs = new QuestionService();
-        qs.displayQuestions();
+        qs.playQuiz();
     }
 }
